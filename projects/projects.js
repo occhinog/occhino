1715a408bdfa5058e87d97ad131e5ac7c38f1764
occhinog <gabrielocchino@icloud.com>
2026-07-07 22:38:57 +0200

    Delete projects directory

--- projects/projects.js
+++
@@ -1,5 +0,0 @@
-const projects = [
-    { name: "Caffetteria", link: "caffetteria.occhino.it" },
-    { name: "Elezioni2018", link: "elezioni18/" },
-    { name: "Elezioni2023", link: "elezioni23/" }
-];
